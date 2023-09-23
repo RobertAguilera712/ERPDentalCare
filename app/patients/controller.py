e866a22d0f502bffbed5e8b04793feca63bb87fd
Roberto Aguilera <rc7.aguilera@gmail.com>
2023-09-23 10:21:33 -0600

    patients completed

--- app/patients/controller.py
+++ app/patients/controller.py
@@ -38,3 +38,47 @@ class PatientsListAPI(Resource):
         db.session.add(patient)
         db.session.commit()
         return patient, 201
+
+@patients_ns.route("/patients/<int:id>")
+class PatientsApi(Resource):
+    @patients_ns.marshal_with(patient_response)
+    def get(self, id):
+        patient = Patient.query.get_or_404(id)
+        return patient
+
+
+    @patients_ns.expect(patient_request)
+    @patients_ns.marshal_with(patient_response)
+    def put(self, id):
+        patient = Patient.query.get_or_404(id)
+
+        person_dict = patients_ns.payload['person']
+        for key, value in person_dict.items():
+            setattr(patient.person, key, value)
+
+        user_dict = patients_ns.payload['user']
+        for key, value in user_dict.items():
+            setattr(patient.user, key, value)
+
+        allergies_ids = [ allergy['id'] for allergy in patients_ns.payload["allergies"] if allergy['id'] != 0]
+        selected_allergies = Allergy.query.filter(Allergy.id.in_(allergies_ids)).all()
+
+        allergies_names = [allergy['name'] for allergy in patients_ns.payload["allergies"] if allergy['id'] == 0]
+        for name in allergies_names:
+            allergy = Allergy(name=name)
+            db.session.add(allergy)
+            selected_allergies.append(allergy)
+
+        patient.allergies = []
+        patient.allergies.extend(selected_allergies)
+        db.session.commit()
+        return patient, 201
+
+
+    def delete(self, id):
+        patient = Patient.query.get_or_404(id)
+        patient.status = 0
+        db.session.commit()
+        return {}, 204
+
+
