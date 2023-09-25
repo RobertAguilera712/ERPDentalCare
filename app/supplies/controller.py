b419ded2b5b8cc7203e49544d39a96d5ff60989d
Roberto Aguilera <rc7.aguilera@gmail.com>
2023-09-25 13:40:51 -0600

    supplies completed

--- app/supplies/controller.py
+++ app/supplies/controller.py
@@ -21,3 +21,31 @@ class SupplyListAPI(Resource):
         db.session.add(supply)
         db.session.commit()
         return supply, 201
+
+
+@supplies_ns.route("/supplies/<int:id>")
+class SupplyApi(Resource):
+
+    @supplies_ns.marshal_with(supply_response)
+    def get(self, id):
+        supply = Supply.query.get_or_404(id)
+        return supply
+
+    @supplies_ns.expect(supply_request)
+    @supplies_ns.marshal_with(supply_response)
+    def put(self, id):
+        supply = Supply.query.get_or_404(id)
+
+        supply_dict = supplies_ns.payload
+
+        for key, value in supply_dict.items():
+            setattr(supply, key, value)
+
+        db.session.commit()
+        return supply, 201
+
+    def delete(self, id):
+        supply = Supply.query.get_or_404(id)
+        supply.status = 0
+        db.session.commit()
+        return {}, 204
